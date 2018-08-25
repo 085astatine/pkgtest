52a713a3838cbbe260cc60df01abfcf8f0d5f5a3
Astatine <astatine085@gmail.com>
2018-08-25 13:53:48 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pkgtest',
+    version='0.0.0',
+    author='Astatine',
+    author_email='astatine085@gmail.com',
+    url='https://github.com/085astatine/pkgtest',
+    packages=['pkgtest'])
